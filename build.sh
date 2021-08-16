59d082ad812f2aaf7b1775785be6330d07f0441d
ugjka <esesmu@protonmail.com>
2021-08-16 20:52:59 +0300

    build

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+make
+find build \( -name '*.css' -o -name '*.html' -o -name '*.js' -o -name '*.json' -o -name '*.txt' -o -name '*.xml' -o -name '*.yml' \) -exec gzip --best --keep -f {} \;
+rsync -Pvhr --delete build/ ugjka.net:share/_/
