885be4ddd9af83bc478c24469f3d1b01cf22b6b8
ugjka <ugjka@proton.me>
2024-02-27 19:25:28 +0200

    2024

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 make
 find build \( -name '*.css' -o -name '*.html' -o -name '*.js' -o -name '*.json' -o -name '*.txt' -o -name '*.xml' -o -name '*.yml' \) -exec gzip --best --keep -f {} \;
-rsync -Pvhr --delete build/ ugjka.net:share/_/
+rsync -Pvhr --delete build/ ugjka.net:share/33ee50d0167438e44d106c1e82b10ab203ba04e6/
